e143245ad725301c146695810e0b9a55f767dfc9
koushik94949 <48681544+koushik94949@users.noreply.github.com>
2019-06-09 19:28:56 +0530

    Update TestNG.java

--- project/src/test/java/artifactory/test/TestNG.java
+++ project/src/test/java/artifactory/test/TestNG.java
@@ -3,7 +3,6 @@ package project;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.By.ByXPath;
-import org.openqa.jetty.html.Select;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
