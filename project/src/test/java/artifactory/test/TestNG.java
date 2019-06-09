d6bb83573e49e72b216cf71352bbfc7788fa40ce
koushik94949 <48681544+koushik94949@users.noreply.github.com>
2019-06-09 18:54:00 +0530

    Create TestNG.java

---
+++ project/src/test/java/artifactory/test/TestNG.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByXPath;
+import org.openqa.jetty.html.Select;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestNG {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://35.200.147.21:31000/project-1.0-RAMA/");
+        //driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+     
+    }
+    
+  }
