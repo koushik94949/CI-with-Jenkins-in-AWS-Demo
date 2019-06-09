b0c35992cdd5b3f939bd2a8c3512792231e61f1c
koushik94949 <48681544+koushik94949@users.noreply.github.com>
2019-06-09 19:27:28 +0530

    Update TestNG.java

--- project/src/test/java/artifactory/test/TestNG.java
+++ project/src/test/java/artifactory/test/TestNG.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TestNG {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "C:\Program Files(x86)\Google\Chrome\Application\chrome.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://35.200.147.21:31000/project-1.0-RAMA/");
         //driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
