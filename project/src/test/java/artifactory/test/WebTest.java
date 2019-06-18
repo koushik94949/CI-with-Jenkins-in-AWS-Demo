170f8f34e6456c85889d94e4f422a6cfbc885a70
koushik94949 <48681544+koushik94949@users.noreply.github.com>
2019-06-18 19:55:30 +0530

    Create WebTest.java

---
+++ project/src/test/java/artifactory/test/WebTest.java
@@ -0,0 +1,27 @@
+package junitwebapp;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkPresent;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertTitleEquals;
+import static net.sourceforge.jwebunit.junit.JWebUnit.beginAt;
+import static net.sourceforge.jwebunit.junit.JWebUnit.clickLink;
+import static net.sourceforge.jwebunit.junit.JWebUnit.setBaseUrl;
+import static net.sourceforge.jwebunit.junit.JWebUnit.setTestingEngineKey;
+ 
+import org.junit.Before;
+import org.junit.Test;
+ 
+import net.sourceforge.jwebunit.util.TestingEngineRegistry;
+ 
+ 
+public class WebTest {
+    @Before
+    public void prepare() {
+        setTestingEngineKey(TestingEngineRegistry.TESTING_ENGINE_HTMLUNIT); 
+        setBaseUrl("http://35.200.147.21:31000/project-1.0-RAMA/");
+    }
+ 
+    @Test
+    public void testLoginPage() {
+        beginAt("index.html"); 
+        assertTitleEquals("Hello DevOps Engineers and Architects!");
+    }
+}
