12a40f96a1088e03d53423af7edb91b3a3256555
koushik94949 <48681544+koushik94949@users.noreply.github.com>
2019-06-18 08:17:46 +0530

    JWebUnitTest

---
+++ project/src/test/java/artifactory/test/JWebUnitTest.java
@@ -0,0 +1,29 @@
+package junitwebapp;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkPresent;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertTitleEquals;
+import static net.sourceforge.jwebunit.junit.JWebUnit.beginAt;
+import static net.sourceforge.jwebunit.junit.JWebUnit.clickLink;
+import static net.sourceforge.jwebunit.junit.JWebUnit.setBaseUrl;
+import static net.sourceforge.jwebunit.junit.JWebUnit.setTestingEngineKey;
+ 
+import org.junit.Before;
+import org.junit.Test;
+ 
+import net.sourceforge.jwebunit.util.TestingEngineRegistry;
+ 
+ 
+public class JWebUnitTest {
+    @Before
+    public void prepare() {
+        setTestingEngineKey(TestingEngineRegistry.TESTING_ENGINE_HTMLUNIT); 
+        setBaseUrl("http://localhost:8080/junitwebapp");
+    }
+ 
+    @Test
+    public void testLoginPage() {
+        beginAt("index.html"); 
+        assertTitleEquals("Welcome to Worldclass DevOps Workshop batch2 ");
+        assertTitleEquals("Hello  DevOps Engineers and Architects!");
+    }
+     
+}
