54171a88666cfb67abe12bee3a7a86c71f4581f2
koushik94949 <48681544+koushik94949@users.noreply.github.com>
2019-06-22 02:19:31 -0400

    Update TestNG.java

--- project/src/test/java/artifactory/test/TestNG.java
+++ project/src/test/java/artifactory/test/TestNG.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+@test
 public class TestNG  {
     public static void main(String[] args) {
         // Create a new instance of the Firefox driver
@@ -14,24 +15,24 @@ public class TestNG  {
         // not the implementation.
         System.setProperty("webdriver.gecko.driver", "/usr/bin/firefox");
         WebDriver driver = new FirefoxDriver();
-        @test
+        
         // And now use this to visit Google
         driver.get("http://www.google.com");
         // Alternatively the same thing can be done like this
         // driver.navigate().to("http://www.google.com");
-        @test
+       
         // Find the text input element by its name
         WebElement element = driver.findElement(By.name("q"));
-        @test
+       
         // Enter something to search for
         element.sendKeys("Cheese!");
-        @test
+       
         // Now submit the form. WebDriver will find the form for us from the element
         element.submit();
         
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
-        @test
+      
         // Google's search is rendered dynamically with JavaScript.
         // Wait for the page to load, timeout after 10 seconds
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
