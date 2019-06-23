6055a688f16df93758cf8adab1e1c20bb2092c6c
koushik94949 <48681544+koushik94949@users.noreply.github.com>
2019-06-23 15:50:45 +0530

    Update SeleniumTest.java

--- project/src/test/java/artifactory/test/SeleniumTest.java
+++ project/src/test/java/artifactory/test/SeleniumTest.java
@@ -6,7 +6,7 @@ import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.GeckoDriver;
 
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -14,18 +14,18 @@ public class SeleniumTest {
 	private static WebDriver driver;
 
 	public SeleniumTest() {
-		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		System.setProperty("webdriver.firefox.driver", "/usr/bin/geckodriver");
 		
 	}
 
 	@Test
 	public void isPageTitleCorrect() throws InterruptedException {
-		driver = new ChromeDriver();
+		driver = new GeckoDriver();
 		driver.get("https://www.google.com/xhtml");
 		driver.manage().window().maximize();
 		Thread.sleep(5000);
 		WebElement searchBox = driver.findElement(By.name("q"));
-		searchBox.sendKeys("ChromeDriver");
+		searchBox.sendKeys("GeckoDriver");
 		searchBox.submit();
 		Thread.sleep(5000); // Let the user actually see something!
 		driver.quit();
