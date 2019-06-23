dd9406a6f78ad30de9eac5ed8039886a3910b943
koushik94949 <48681544+koushik94949@users.noreply.github.com>
2019-06-23 13:49:05 +0530

    Update SeleniumTest.java

--- project/src/test/java/artifactory/test/SeleniumTest.java
+++ project/src/test/java/artifactory/test/SeleniumTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome_options;
+
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SeleniumTest {
@@ -22,10 +22,6 @@ public class SeleniumTest {
 	public void isPageTitleCorrect() throws InterruptedException {
 		
 		driver = new ChromeDriver();
-		chrome_options = Options();
-		chrome_options.add_argument('--headless');
-		chrome_options.add_argument('--no-sandbox');
-		chrome_options.add_argument('--disable-dev-shm-usage');
 		driver.get("https://www.google.com/xhtml");
 		driver.manage().window().maximize();
 		Thread.sleep(5000);
