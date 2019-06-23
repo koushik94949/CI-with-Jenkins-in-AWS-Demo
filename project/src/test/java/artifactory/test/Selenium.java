1dfa23b5936b89e2d4c649e0646f14f620451fb8
koushik94949 <48681544+koushik94949@users.noreply.github.com>
2019-06-23 17:34:49 +0530

    Update Selenium.java

--- project/src/test/java/artifactory/test/Selenium.java
+++ project/src/test/java/artifactory/test/Selenium.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SeleniumTest {
