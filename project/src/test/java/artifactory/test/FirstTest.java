6441d110c5e2d7ea529d41dea5f34b17431051cf
koushik94949 <48681544+koushik94949@users.noreply.github.com>
2019-06-24 10:34:56 +0530

    Update FirstTest.java

--- project/src/test/java/artifactory/test/FirstTest.java
+++ project/src/test/java/artifactory/test/FirstTest.java
@@ -20,7 +20,7 @@ public class FirstTest {
     //-----------------------------------Test Setup-----------------------------------
     @BeforeMethod
     public void setupTest (){
-        System.setProperty("webdriver.chrome.driver","/usr/bin/");
+        System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver/");
         //Create a new ChromeDriver
         driver = new ChromeDriver();
  
